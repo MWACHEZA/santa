2083f008ae9294e895fdcf53df747265e7a7c2f0
comforter <comforter958@gmail.com>
2026-05-11 17:13:47 +0200

    Allow x-access-token CORS header in backend to resolve client POST and file upload blocks

--- backend/server.js
+++ backend/server.js
@@ -99,7 +99,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-access-token']
 }));
 
 // Body parsing middleware
